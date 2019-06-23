beafa068c8f4a909f42d73d9d2afac81c72b209a
nikita202 <nikita@ostaplyuk.com>
2019-06-23 16:09:19 +0300

    commands added

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const Telegram = require('telegram-bot-api');
 const CHANNEL_ID = "fridrixxx";
 const GROUP_ID = -1001366233526;
+const blocked = [];
+
+Array.prototype.remove = function() {
+    let what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
 let args = {};
 for (let arg of process.argv.slice(2)) {
     let arr = arg.split("=");
@@ -21,28 +34,97 @@ const api = new Telegram({
     }
 });
 
+function replyToMessage(msg, text) {
+    api.sendMessage({
+        chat_id: msg.chat.id,
+        text: text,
+        reply_to_message_id: msg.message_id
+    });
+}
+
+const CommandProcessor = new (require('./commandsprocessor'))([
+    {
+        name: "help",
+        description: "Helps with understanding commands :)",
+        usage: "help ?command",
+        action: function (msg, arguments, self) {
+            if (arguments.length === 0) {
+                let string = 'All commands list:\n\t';
+                let commandNames = [];
+                self.commands.forEach(command => {commandNames.push(command.name)});
+                string += commandNames.join('\n\t');
+                msg.reply(string);
+            } else if (arguments.length === 1) {
+                let command = arguments[0].value;
+                let commandObject;
+                self.commands.forEach(comm => {
+                    if (comm.name === command.toLowerCase()) commandObject = comm;
+                });
+                if (typeof commandObject === "undefined") throw {message: "Such a command doesn't exist"};
+                let string = "Command name: " + commandObject.name + "\nCommand description: " + commandObject.description + "\nCommand usage: " + commandObject.usage + (commandObject.adminOnly ? "\nAdmin only!" : "");
+                replyToMessage(msg, string)
+            }
+        }
+    },
+    {
+        name: "block",
+        description: "Restricts user from sending suggestions",
+        usage: "block <user id>",
+        action: function (msg, arguments, self) {
+            if (arguments.length === 0) {
+                replyToMessage(msg, "Not enough arguments!")
+            } else {
+                let toBlock = arguments[0].value;
+                if (!isNaN(parseInt(toBlock))) {
+                    blocked.push(parseInt(toBlock));
+                } else replyToMessage("Wrong 1st argument type");
+            }
+        }
+    },
+    {
+        name: "unblock",
+        description: "Returns the right to send messages",
+        usage: "unblock <user id>",
+        action: function (msg, arguments, self) {
+            if (arguments.length === 0) {
+                replyToMessage(msg, "Not enough arguments!")
+            } else {
+                let toBlock = arguments[0].value;
+                if (!isNaN(parseInt(toBlock))) {
+                    blocked.remove(parseInt(toBlock));
+                } else replyToMessage("Wrong 1st argument type");
+            }
+        }
+    }
+]);
+
 api.on('message', function (message) {
     if (args.debug === 'true') console.log(message);
     if (message.chat.type === "private") {
         if (message.text !== "/start") {
-            api.sendMessage({
-                chat_id: message.chat.id,
-                text: "Спасибо за предложение!",
-                reply_to_message_id: message.message_id
-            });
-            api.sendMessage({
-                chat_id: GROUP_ID,
-                text: "Предложение от " + generateString(message.chat)
-            });
-            api.forwardMessage({
-                chat_id: GROUP_ID,
-                from_chat_id: message.chat.id,
-                message_id: message.message_id
-            });
+            if (!blocked.some(id => id == message.chat.id)) {
+                replyToMessage(message, "Спасибо за предложение!");
+                api.sendMessage({
+                    chat_id: GROUP_ID,
+                    text: "Предложение от " + generateString(message.chat)
+                });
+                api.forwardMessage({
+                    chat_id: GROUP_ID,
+                    from_chat_id: message.chat.id,
+                    message_id: message.message_id
+                });
+            } else {
+                replyToMessage(message, "Вы заблокированы! Обращайтесь к админам @" + CHANNEL_ID)
+            }
         } else api.sendMessage({
             chat_id: message.chat.id,
             text: "Здравствуйте!\nЭто бот-предложка канала Фридрих IV (@" + CHANNEL_ID + ").\nКидайте сюда свои смехуечки.",
         })
+    } else if (message.chat.id == GROUP_ID) {
+        if (message.text.startsWith("/")) {
+            let command = message.text.slice(1);
+            CommandProcessor.process(command, message);
+        }
     }
 });
 
@@ -51,5 +133,6 @@ function generateString(chat) {
     if (chat.first_name) message += chat.first_name + " ";
     if (chat.last_name) message += chat.last_name + " ";
     if (chat.username) message += "@" + chat.username + " ";
+    if (chat.id) message += "(id: " + chat.id + ") ";
     return message;
 }
