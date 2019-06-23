2110f4c6ddcdc92f2ab856809259f726f905f0b8
nikita202 <nikita@ostaplyuk.com>
2019-06-23 14:53:26 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const Telegram = require('telegram-bot-api');
+const CHANNEL_ID = "fridrixxx";
+const GROUP_ID = -1001366233526;
+
+const api = new Telegram({
+    token: '830368677:AAFV5f2EtaGi1BQ-tuLeUM_U5FhQZbDKHrs',
+    updates: {
+        enabled: true
+    }
+});
+
+api.on('message', function (message) {
+    console.log(message);
+    if (message.chat.type === "private") {
+        if (message.text !== "/start") {
+            api.sendMessage({
+                chat_id: message.chat.id,
+                text: "Спасибо за предложение!",
+                reply_to_message_id: message.message_id
+            });
+            api.sendMessage({
+                chat_id: GROUP_ID,
+                text: "Предложение от " + generateString(message.chat, message.from)
+            });
+            api.forwardMessage({
+                chat_id: GROUP_ID,
+                from_chat_id: message.chat.id,
+                message_id: message.message_id
+            });
+        } else api.sendMessage({
+            chat_id: message.chat.id,
+            text: "Здравствуйте!\nЭто бот-предложка канала Фридрих IV (@fridrixxx).\nКидайте сюда свои смехуечки.",
+        })
+    }
+});
+
+function generateString(chat, from) {
+    let message = "";
+    if (chat.first_name) message += chat.first_name + " ";
+    if (chat.last_name) message += chat.last_name + " ";
+    if (chat.username) message += "@" + chat.username + " ";
+    return message;
+}
